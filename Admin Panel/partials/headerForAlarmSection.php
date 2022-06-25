e9a9645b24322c302c874b6aa9c33f112d2f9977
Parasite-231 <muktadir@iut-dhaka.edu>
2022-06-25 19:43:56 +0600

    refactored

--- Admin Panel/partials/headerForAlarmSection.php
+++ Admin Panel/partials/headerForAlarmSection.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" type="image/x-icon" href="../../ICONS/adminsign-upicon.png">
     <link rel="stylesheet" href="../CSS Files/Admin Panel Designs/AdminDashboardDesign.css">
+    <link rel="stylesheet" href="../../CSS Files/Admin Panel Designs/slideBar.css">
 
     <link href="https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
@@ -40,7 +41,7 @@
     </script>
 
     <script src="../../js/printerBot.js"></script>
-    <link rel="stylesheet" href="../../CSS Files/Admin Panel Designs/slideBar.css">
+
 
 
     <title>Alarm Message</title>
