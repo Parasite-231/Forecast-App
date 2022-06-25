436bacbef8eff8de8c3263892d78a753db13b7d6
Parasite-231 <muktadir@iut-dhaka.edu>
2022-06-25 17:04:13 +0600

    fns

--- Admin Panel/function.php
+++
@@ -1,8 +0,0 @@
-<?php
-include("../databaseConnection.php");
-function newDate($day)
-{
-$date = date('Y-m-d', time() + 4 * 3600);
-echo date('Y-m-d', strtotime($date . ' + ' . $day . 'days'));
-}
-?>
